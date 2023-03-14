5e60982e297f9cfa05530be83bbb0fbf4447d96c
geislerovamarie <geislerova.marie@gmail.com>
2023-03-14 18:26:21 +0100

    Settings modified

--- app/src/main/java/com/example/spravanot/MainActivity.java
+++ app/src/main/java/com/example/spravanot/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.spravanot;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.LocaleListCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.preference.PreferenceManager;
 
 import com.example.spravanot.databinding.ActivityMainBinding;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +46,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // settings - turning off display, language, darkmode
+        setSettings();
     }
 
     @Override
@@ -53,6 +72,36 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    void setSettings(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // display
+        boolean display_on_pref = sp.getBoolean("display_on_off", false);
+        displaySetting(display_on_pref);
+
+        // language
+        String language = sp.getString("language","");
+        setLanguage(language);
+
+
+    }
+
+    void displaySetting(boolean display_on_pref){
+        if(display_on_pref){
+            // Toast.makeText(this, "The display should stay on", Toast.LENGTH_SHORT).show();
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            // Toast.makeText(this, "The display should turn off eventually", Toast.LENGTH_SHORT).show();
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    void setLanguage(String language){
+        LocaleListCompat appLocale = LocaleListCompat.forLanguageTags(language);
+        AppCompatDelegate.setApplicationLocales(appLocale);
+        // Toast.makeText(this, "language: " + language, Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/example/spravanot/SettingsActivity.java
+++ app/src/main/java/com/example/spravanot/SettingsActivity.java
@@ -1,10 +1,15 @@
 package com.example.spravanot;
 
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+
+import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -24,10 +29,22 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    public static void setLocale(Activity activity, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Resources resources = activity.getResources();
+        Configuration config = resources.getConfiguration();
+        config.setLocale(locale);
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+    }
+
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
+
+
+
 }
